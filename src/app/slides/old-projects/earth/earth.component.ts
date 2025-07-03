572bc25ba8e51a37e9113642612ef9a28962b42f
Bay, Jason <Jason.Bay@adesso.de>
2025-07-03 10:31:45 +0200

    chaged earth gif with real website

--- src/app/slides/old-projects/earth/earth.component.ts
+++ src/app/slides/old-projects/earth/earth.component.ts
@@ -1,30 +1,10 @@
-import { Component, ElementRef, ViewChild  } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-earth',
-  imports: [],
   templateUrl: './earth.component.html',
   styleUrls: ['./earth.component.css']
 })
 export class EarthComponent {
-
-  @ViewChild('tiltImage', { static: true }) imageRef!: ElementRef<HTMLImageElement>;
-
-onTilt(event: MouseEvent) {
-  const image = this.imageRef.nativeElement;
-  const bounds = image.getBoundingClientRect();
-  const centerX = bounds.left + bounds.width / 2;
-  const centerY = bounds.top + bounds.height / 2;
-
-  const rotateX = -((event.clientY - centerY) / 50);
-  const rotateY = ((event.clientX - centerX) / 50);
-
-  image.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-}
-
-onTiltLeave(event: MouseEvent) {
-  const image = this.imageRef.nativeElement;
-  image.style.transform = 'rotateX(0deg) rotateY(0deg)';
-}
-
-}
+  
+ }
